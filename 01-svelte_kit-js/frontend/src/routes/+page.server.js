b3326cc9b046f094b39b21a0efa925da48e7e60b
CassianKnoth <cassian.knoth@accenture.com>
2022-10-18 09:45:18 +0200

    feat: added .env to frontend for cms api base url

--- 01-svelte_kit-js/frontend/src/routes/+page.server.js
+++ 01-svelte_kit-js/frontend/src/routes/+page.server.js
@@ -1,5 +1,7 @@
 export const load = async () => {
-  const res = await fetch("http://localhost:1337/api/posts?populate=*");
+  const res = await fetch(
+    `${import.meta.env.VITE_CMS_URL}/api/posts?populate=*`
+  );
   const entries = await res.json();
   return { entries: entries.data };
 };
